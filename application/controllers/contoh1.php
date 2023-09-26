43b1e14b21a4205f3e758dd15c7ce63f284d1da3
videlisagung <videlisagung@gmail.com>
2023-09-26 20:47:00 +0700

    Membuat project CI 3 Pustaka Booking

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,16 @@
+<?php
+class contoh1 extends CI_Controller
+{
+  public function index()
+  {
+  echo "<h1> perkenalkan </h1>";
+  echo "Nama Saya Videlis Agung
+  saya tinggal di Slipi
+  olahraga yang saya sukai adalah volly";
+
+   }
+
+
+}
+
+?>
